cae2a375abfec0072ec6b33db30afebd3830fe53
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2025-11-28 19:52:25 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
