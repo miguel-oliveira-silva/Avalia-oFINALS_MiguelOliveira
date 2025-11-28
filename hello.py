aec19b5a0e7b175cb823e8d64736db012c42ea5a
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2025-11-28 19:39:00 -0300

    Simplify index route by removing user logic
    Removed user and role handling from index route.

--- hello.py
+++ hello.py
@@ -65,22 +65,4 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    user_all = User.query.all();
-    role_all = Role.query.all();
-    print(user_all);
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()                        
-        if user is None:
-            user_role = Role.query.filter_by(name=form.role.data).first();
-            user = User(username=form.name.data, role=user_role);
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           user_all=user_all, role_all = role_all);
+    return render_template('index.html');
